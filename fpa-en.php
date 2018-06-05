b9048481da886ff3b5ef7132f8c6e1dcaa28bb85
Sveinung Larsen <frostmakk@users.noreply.github.com>
2018-06-05 21:59:27 +0200

    Update fpa-en.php

--- fpa-en.php
+++ fpa-en.php
@@ -8,7 +8,7 @@
 /**
  **  @package Forum Post Assistant / Bug Report Assistant
  **  @version 1.4.1
- **  @last updated 17/05/2018
+ **  @last updated 05/06/2018
  **  @release Beta
  **  @date 24/06/2011
  **  @author RussW
@@ -35,7 +35,7 @@
 
 	define ( '_RES', 'Forum Post Assistant' );
 	define ( '_RES_VERSION', '1.4.1 (Lippy)' );
-	define ( '_last_updated', '05/05/2018' );
+	define ( '_last_updated', '05-June-2018' );
 	define ( '_COPYRIGHT_STMT', ' Copyright &copy 2011-'. date("Y").  ' Russell Winter, Phil DeGruy, Bernard Toplak, Claire Mandville, Sveinung Larsen. <br>' );
 	define ( '_LICENSE_LINK', '<a href="http://www.gnu.org/licenses/" target="_blank">http://www.gnu.org/licenses/</a>' ); // link to GPL license
 	define ( '_LICENSE_FOOTER', ' The FPA comes with ABSOLUTELY NO WARRANTY. <br> This is free software,

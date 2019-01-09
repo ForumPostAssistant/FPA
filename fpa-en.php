3cc6b915d923f2bd8937affbbf87cd35075a232f
mandville <mandville@users.noreply.github.com>
2019-01-09 09:19:52 +0000

    Update fpa-en.php

--- fpa-en.php
+++ fpa-en.php
@@ -7,8 +7,8 @@
 
 /**
  **  @package Forum Post Assistant / Bug Report Assistant
- **  @version 1.4.6 BabyK
- **  @last updated 12/12/2018
+ **  @version 1.4.7 litoralis 
+ **  @last updated 09/01/2019
  **  @release Beta
  **  @date 24/06/2011
  **  @author RussW
@@ -34,7 +34,7 @@
 	// Define some basic assistant information
 
 	define ( '_RES', 'Forum Post Assistant' );
-	define ( '_RES_VERSION', '1.4.6 (BabyK)' );
+	define ( '_RES_VERSION', '1.4.7 (litoralis)' );
 	define ( '_last_updated', '12-Dec-2018' );
 	define ( '_COPYRIGHT_STMT', ' Copyright &copy 2011-'. @date("Y").  ' Russell Winter, Phil DeGruy, Bernard Toplak, Claire Mandville, Sveinung Larsen. <br>' );
 	define ( '_LICENSE_LINK', '<a href="http://www.gnu.org/licenses/" target="_blank">http://www.gnu.org/licenses/</a>' ); // link to GPL license

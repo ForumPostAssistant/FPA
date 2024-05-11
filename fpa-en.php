a63c75d89c31e78d5374db15c7719856cb27ed92
RussW <871931+RussW@users.noreply.github.com>
2024-05-11 19:47:19 +1000

    v1.6.7 Release Preparation
    updated versioning for v1..6.7 release (Improved J5.1 support)

--- fpa-en.php
+++ fpa-en.php
@@ -1,6 +1,6 @@
 <?php
     /**
-     * @version 1.6.6
+     * @version 1.6.7
      * @package Joomla!
      * @subpackage Forum Post Assistant
      * @category Diagnostic Tool
@@ -21,10 +21,10 @@
      *
      */
      define ( '_RES', 'Forum Post Assistant' );
-     define ( '_RES_VERSION', '1.6.6' );
-     define ( '_RES_CODENAME', 'Betelgeuse' );
-     define ( '_RES_LAST_UPDATED', '19-Dec-2022' );
-     define ( '_RES_RELEASE', 'Stable' );              // can be Alpha, Beta, RC, Stable
+     define ( '_RES_VERSION', '1.6.7' );
+     define ( '_RES_CODENAME', 'Marvin' );
+     define ( '_RES_LAST_UPDATED', '11-May-2024' );
+     define ( '_RES_RELEASE', 'Stable' );             // can be Alpha, Beta, RC, Stable
      define ( '_RES_LANG', 'en-GB' );                 // Country/Language Code
      define ( '_RES_COPYRIGHT_STMT', ' Copyright &copy; 2011-'. @date("Y").  ' Russell Winter, Phil DeGruy, Bernard Toplak, Claire Mandville, Sveinung Larsen. <br>' );
 

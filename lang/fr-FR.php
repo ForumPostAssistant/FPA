d2815baf6eadbee65d774e47438c294bb2d3737a
Russ Winter <winterrg@gmail.com>
2026-06-16 21:59:58 +1000

    create lang folder and test fr-FR translation file

---
+++ lang/fr-FR.php
@@ -0,0 +1,48 @@
+<?php
+// fr-FR language strings
+return [
+    // FPA Core
+    'THISLANG'          => 'fr-FR',
+    'FPALONG'           => 'Forum Post Assistant',
+    'FPASHORT'          => 'FPA',
+    'FPADESC'           => 'Outil d\'assistance pour les messages de forum et d\'audit de diagnostic pour le CMS Joomla!.',
+
+    // FPA statuses, results & descriptors
+    'YES'               => 'Oui',
+    'NO'                => 'Non',
+    'UNKNOWN'           => 'Inconnu',
+    'NA'                => 'N/A',
+    'WARNING'           => 'Avertissement',
+    'ERROR'             => 'Erreur',
+    'SUCCESS'           => 'Succès',
+    'INFO'              => 'Info',
+    'FAILED'            => 'Échoué',
+    'OK'                => 'OK',
+    'TRUE'              => 'Vrai',
+    'FALSE'             => 'Faux',
+    'MAYBE'             => 'Peut-être',
+    'DEFUNCT'           => 'Obsolète',
+    'STATUS'            => 'Statut',
+    'CURRENT'           => 'Actuel',
+    'LATEST'            => 'Dernier',
+    'DEVBUILD'          => 'Version de dév',
+    'UPTODATE'          => 'À jour',
+    'UPDATEAVAIL'       => 'Mise à jour disponible',
+    'PLATFORM'          => 'Plateforme',
+    'ENVIRONMENT'       => 'Environnement', // Corrected English typo 'Enviroment' to French spelling
+    'HOST'              => 'Hôte',
+    'SERVER'            => 'Serveur',
+    'APPLICATION'       => 'Application',
+    'WEB'               => 'Web',
+    'PHP'               => 'PHP',
+    'DBASE'             => 'Base de données',
+
+    // FPA titles, headings & descriptions
+    'RUNTIMEOPTIONS'    => 'Options d\'exécution',
+    'KEYMETRICS'        => 'Indicateurs clés',
+    'CONFIDENCE'        => 'Confiance',
+    'CONFIGURATION'     => 'Configuration',
+    'CONFIG'            => 'Config',
+
+    // End-user messages and textual content
+];

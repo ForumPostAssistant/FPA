e9462eedcf0b88f37aa38f0ddef16b87e5f782cf
Russ Winter <winterrg@gmail.com>
2026-06-17 16:21:22 +1000

    Create de-DE.php

---
+++ lang/de-DE.php
@@ -0,0 +1,47 @@
+<?php
+// Standard de-DE Sprach-Strings
+return [
+    // FPA Core
+    'THISLANG'          => 'de-DE',
+    'FPALONG'           => 'Forum Post Assistant',
+    'FPASHORT'          => 'FPA',
+    'FPADESC'           => 'Forum Post Assistant und Diagnose-Audit-Tool für das Joomla! CMS.',
+    // FPA statuses, results & descriptors
+    'YES'               => 'Ja',
+    'NO'                => 'Nein',
+    'UNKNOWN'           => 'Unbekannt',
+    'NA'                => 'N/V',
+    'WARNING'           => 'Warnung',
+    'ERROR'             => 'Fehler',
+    'SUCCESS'           => 'Erfolgreich',
+    'INFO'              => 'Info',
+    'FAILED'            => 'Fehlgeschlagen',
+    'OK'                => 'OK',
+    'TRUE'              => 'Wahr',
+    'FALSE'             => 'Falsch',
+    'MAYBE'             => 'Vielleicht',
+    'DEFUNCT'           => 'Veraltet',
+    'STATUS'            => 'Status',
+    'CURRENT'           => 'Aktuell',
+    'LATEST'            => 'Neueste',
+    'DEVBUILD'          => 'Entwicklungsversion',
+    'UPTODATE'          => 'Aktuell',
+    'UPDATEAVAIL'       => 'Update verfügbar',
+    'PLATFORM'          => 'Plattform',
+    'ENVIRONMENT'       => 'Umgebung',
+    'HOST'              => 'Host',
+    'SERVER'            => 'Server',
+    'APPLICATION'       => 'Anwendung',
+    'WEB'               => 'Web',
+    'PHP'               => 'PHP',
+    'DBASE'             => 'Datenbank',
+
+    // FPA titles, headings & descriptions
+    'RUNTIMEOPTIONS'    => 'Laufzeitoptionen',
+    'KEYMETRICS'        => 'Schlüsselkennzahlen',
+    'CONFIDENCE'        => 'Vertrauenswürdigkeit',
+    'CONFIGURATION'     => 'Konfiguration',
+    'CONFIG'            => 'Konfig',
+
+    // End-user messages and textual content
+];
